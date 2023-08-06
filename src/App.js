9d2a849699ea090ca6d4656072283de95f38e78f
stealblue <melancholyhotdog@gmail.com>
2023-08-06 20:37:22 +0900

    메인페이지 테스트

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
 
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        stealblue gitblog
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      stealblue git blog
+      <div><a href="https://www.google.com" target="_blank"></a></div>
     </div>
   );
 }
